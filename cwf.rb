a1d0410729af3a3ea31b48a63122c273663a712f
大塚智貴 <moppi@ootsukasatoshitakanoMacBook-Pro.local>
2023-07-17 15:06:32 +0900

    cwf.rbの追加

---
+++ cwf.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Cwf < Formula
+  desc "Weather CLI via https://open-meteo.com/, etc."
+  homepage "https://github.com/Moppi0725/cwf"
+  url "https://github.com/Moppi0725/cwf/releases#:~:text=cwf%2D0.1.16_darwin_amd64.tar.gz"
+  version "0.1.16"
+  sha256 "379339c996e82134c46fb6cfebbae49baf879fadae87a341f5ee26defed12b91"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test cwf`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
